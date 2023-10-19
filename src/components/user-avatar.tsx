f56a6d3c5a0399183d1ac25fcc3cfd41cd127ba1
sads4ndwich <caiocamargo007@gmail.com>
2023-10-18 21:52:20 -0300

    use logged user session to set avatar

--- src/components/user-avatar.tsx
+++ src/components/user-avatar.tsx
@@ -1,10 +1,17 @@
+'use client'
+
+import { useSession } from 'next-auth/react'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 
 export function UserAvatar() {
+  const { data } = useSession()
+
+  const fallback = data?.user.name.substring(0, 2)
+
   return (
     <Avatar>
-      <AvatarImage src="https://github.com/SadS4ndWiCh.png" />
-      <AvatarFallback>SA</AvatarFallback>
+      <AvatarImage src={data?.user.picture} />
+      <AvatarFallback>{fallback?.toUpperCase()}</AvatarFallback>
     </Avatar>
   )
 }
