9af418cbf30f68cdc749f41662b36cfb218dd77a
sads4ndwich <caiocamargo007@gmail.com>
2023-10-22 14:15:42 -0300

    added forward ref

--- src/components/user-avatar.tsx
+++ src/components/user-avatar.tsx
@@ -1,17 +1,25 @@
 'use client'
 
+import { forwardRef } from 'react'
 import { useSession } from 'next-auth/react'
+
+import * as AvatarPrimitive from '@radix-ui/react-avatar'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 
-export function UserAvatar() {
+export const UserAvatar = forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
+>(({ ...props }, ref) => {
   const { data } = useSession()
 
   const fallback = data?.user.name.substring(0, 2)
 
   return (
-    <Avatar>
+    <Avatar ref={ref} {...props}>
       <AvatarImage src={data?.user.picture} />
       <AvatarFallback>{fallback?.toUpperCase()}</AvatarFallback>
     </Avatar>
   )
-}
+})
+
+UserAvatar.displayName = 'UserAvatar'
