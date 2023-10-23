a093644ca406e673ca2cb254c315f4c1cac9caf9
sads4ndwich <caiocamargo007@gmail.com>
2023-10-23 17:27:06 -0300

    added url of api util function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function url(route: string) {
+  return new URL(route, process.env.NEXT_PUBLIC_API_BASE_URL)
+}
