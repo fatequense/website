dcdb71512bc97e5ffa3aa3345e911cb141bae51a
sads4ndwich <caiocamargo007@gmail.com>
2023-10-24 17:23:18 -0300

    include some metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,39 @@ import './globals.css'
 import { sans } from './fonts'
 import { Providers } from '../components/providers'
 import { TailwindIndicator } from '~/components/tailwind-indicator'
+import { siteConfig } from '~/config/site'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | Fatequense',
-    default: 'Fatequense',
+    default: `${siteConfig.name}`,
+    template: `%s | ${siteConfig.name}`,
   },
+  description: siteConfig.description,
+  authors: [
+    {
+      name: 'SadS4ndWiCh',
+      url: 'https://github.com/SadS4ndWiCh',
+    },
+  ],
+  creator: 'SadS4ndWiCh',
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
+  openGraph: {
+    type: 'website',
+    locale: 'pt_BR',
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
 }
 
 export default function RootLayout({
