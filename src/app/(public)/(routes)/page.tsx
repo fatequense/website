fd41595a3257eb692b614b7c826ef24318c2e26a
sads4ndwich <caiocamargo007@gmail.com>
2023-10-24 17:32:53 -0300

    feat: set heading content with site description

--- src/app/(public)/(routes)/page.tsx
+++ src/app/(public)/(routes)/page.tsx
@@ -4,13 +4,13 @@ import Link from 'next/link'
 
 import { Button } from '~/components/ui/button'
 import { ArrowRight } from 'lucide-react'
+import { siteConfig } from '~/config/site'
 
 export default function LandingPage() {
   return (
     <main className="flex-1 flex flex-col justify-center space-y-6">
-      <h1 className="max-w-2xl text-3xl font-bold leading-relaxed">
-        Uma plataforma para visualizar os dados de estudante dos alunos da
-        fatec, feita com carinho de fatequense para fatequenses.
+      <h1 className="max-w-3xl text-3xl font-bold leading-relaxed">
+        {siteConfig.description}
       </h1>
       <Button className="w-fit" asChild>
         <Link href="/login">
