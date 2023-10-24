4f8b534421e66912f5e6a9977b84cb85951500f6
sads4ndwich <caiocamargo007@gmail.com>
2023-10-24 16:28:12 -0300

    use dashboard config to render all links

--- src/app/(student)/_components/pages-menu.tsx
+++ src/app/(student)/_components/pages-menu.tsx
@@ -1,13 +1,4 @@
-import {
-  GraduationCap,
-  LayoutGrid,
-  CircleDashed,
-  Star,
-  CalendarDays,
-  GalleryVerticalEnd,
-  SquareStack,
-  Menu,
-} from 'lucide-react'
+import { GraduationCap, Menu } from 'lucide-react'
 import Link from 'next/link'
 import { Button } from '~/components/ui/button'
 import {
@@ -16,6 +7,8 @@ import {
   SheetHeader,
   Sheet,
 } from '~/components/ui/sheet'
+import { dashboardConfig } from '~/config/dashboard'
+import { cn } from '~/lib/utils'
 
 export function PagesMenu() {
   return (
@@ -33,42 +26,22 @@ export function PagesMenu() {
         </SheetHeader>
 
         <div className="h-full mt-4 space-y-2">
-          <Button variant="ghost" className="w-full justify-start" asChild>
-            <Link href="/aluno">
-              <LayoutGrid className="w-4 h-4 mr-2" />
-              <span>Dashboard</span>
-            </Link>
-          </Button>
-          <Button variant="ghost" className="w-full justify-start" asChild>
-            <Link href="/aluno/faltas">
-              <CircleDashed className="w-4 h-4 mr-2" />
-              <span>Faltas</span>
-            </Link>
-          </Button>
-          <Button variant="ghost" className="w-full justify-start" asChild>
-            <Link href="/aluno/notas">
-              <Star className="w-4 h-4 mr-2" />
-              <span>Notas</span>
-            </Link>
-          </Button>
-          <Button variant="ghost" className="w-full justify-start" asChild>
-            <Link href="/aluno/horarios">
-              <CalendarDays className="w-4 h-4 mr-2" />
-              <span>Horários</span>
-            </Link>
-          </Button>
-          <Button variant="ghost" className="w-full justify-start" asChild>
-            <Link href="/aluno/historico">
-              <GalleryVerticalEnd className="w-4 h-4 mr-2" />
-              <span>Histórico</span>
-            </Link>
-          </Button>
-          <Button variant="ghost" className="w-full justify-start" asChild>
-            <Link href="/aluno/historico-grade">
-              <SquareStack className="w-4 h-4 mr-2" />
-              <span>Histórico em Grade</span>
-            </Link>
-          </Button>
+          {dashboardConfig.sidebarNav.map((link) => (
+            <Button
+              key={link.label}
+              variant="ghost"
+              className={cn(
+                'w-full justify-start',
+                link.disabled && 'pointer-events-none text-muted-foreground',
+              )}
+              asChild
+            >
+              <Link href={link.disabled ? '#' : link.href}>
+                <link.icon className="w-4 h-4 mr-2" />
+                {link.label}
+              </Link>
+            </Button>
+          ))}
         </div>
       </SheetContent>
     </Sheet>
