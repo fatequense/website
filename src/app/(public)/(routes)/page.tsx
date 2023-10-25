ebaabe82f990d3e8571492d54dc5e713ea04f531
sads4ndwich <caiocamargo007@gmail.com>
2023-10-25 18:27:10 -0300

    update hero section

--- src/app/(public)/(routes)/page.tsx
+++ src/app/(public)/(routes)/page.tsx
@@ -1,17 +1,21 @@
-'use client'
-
 import Link from 'next/link'
 
-import { Button } from '~/components/ui/button'
 import { ArrowRight } from 'lucide-react'
-import { siteConfig } from '~/config/site'
+
+import { Button } from '~/components/ui/button'
 
 export default function LandingPage() {
   return (
-    <main className="flex-1 flex flex-col justify-center space-y-6">
-      <h1 className="max-w-3xl text-3xl font-bold leading-relaxed">
-        {siteConfig.description}
-      </h1>
+    <main className="min-h-screen flex-1 flex flex-col justify-center space-y-6">
+      <div className="space-y-2">
+        <h1 className="text-2xl sm:text-4xl font-bold leading-relaxed">
+          Seu Portal Acadêmico Pessoal
+        </h1>
+        <p className="max-w-lg text-lg sm:text-xl leading-relaxed text-muted-foreground">
+          Controle seu horário, notas e frequência. Sucesso acadêmico
+          descomplicado para fatequenses.
+        </p>
+      </div>
       <Button className="w-fit" asChild>
         <Link href="/login">
           Entrar como estudante
