97f0a8a9d8c86231da8be6d60e0a6d3a14b47a32
sads4ndwich <caiocamargo007@gmail.com>
2023-10-25 18:25:59 -0300

    align buttons

--- src/app/(public)/_components/nav.tsx
+++ src/app/(public)/_components/nav.tsx
@@ -10,10 +10,10 @@ export function Nav() {
     <nav className="fixed top-0 left-6 right-6 flex justify-between items-center py-4">
       <div className="flex items-center font-bold">
         <GraduationCap className="w-6 h-6 mr-2" />
-        Fatequense
+        <span className="hidden sm:inline-block">Fatequense</span>
       </div>
 
-      <div className="space-x-2">
+      <div className="flex items-center space-x-2">
         <Button asChild>
           <Link href="/login">Entrar</Link>
         </Button>
