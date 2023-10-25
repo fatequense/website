33ad2376fe1f686f13718ebd17754c9c13056f26
sads4ndwich <caiocamargo007@gmail.com>
2023-10-25 18:00:26 -0300

    added student nav links

--- src/config/dashboard.ts
+++ src/config/dashboard.ts
@@ -2,9 +2,12 @@ import {
   CalendarDays,
   CircleDashed,
   GalleryVerticalEnd,
+  Globe,
   LayoutGrid,
+  Settings,
   SquareStack,
   Star,
+  User,
 } from 'lucide-react'
 
 export const dashboardConfig = {
@@ -40,6 +43,25 @@ export const dashboardConfig = {
       href: '/aluno/historico-grade',
       disabled: true,
       icon: SquareStack,
+    },
+  ],
+  studentNav: [
+    {
+      label: 'Perfil',
+      href: '/aluno/perfil',
+      disabled: true,
+      icon: User,
+    },
+    {
+      label: 'Opções',
+      href: '/aluno/opcoes',
+      icon: Settings,
+    },
+    {
+      label: 'SIGA',
+      href: 'https://siga.cps.sp.gov.br/ALUNO/login.aspx',
+      external: true,
+      icon: Globe,
     },
   ],
 }
