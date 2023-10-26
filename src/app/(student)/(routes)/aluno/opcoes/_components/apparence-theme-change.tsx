4a031b77dbaed65be8dfaa2704f84a70561dd768
sads4ndwich <caiocamargo007@gmail.com>
2023-10-25 21:09:59 -0300

    fix hydration error

--- src/app/(student)/(routes)/aluno/opcoes/_components/apparence-theme-change.tsx
+++ src/app/(student)/(routes)/aluno/opcoes/_components/apparence-theme-change.tsx
@@ -1,4 +1,7 @@
 'use client'
+import { useTheme } from 'next-themes'
+
+import { useMounted } from '~/hooks/use-mounted'
 
 import {
   Card,
@@ -7,13 +10,12 @@ import {
   CardHeader,
   CardTitle,
 } from '~/components/ui/card'
-import { useTheme } from 'next-themes'
 import { Label } from '~/components/ui/label'
 import { RadioGroup, RadioGroupItem } from '~/components/ui/radio-group'
-import { cn } from '~/lib/utils'
 
 export function ApparenceThemeChange() {
   const { setTheme, resolvedTheme } = useTheme()
+  const isMounted = useMounted()
 
   return (
     <Card className="w-full">
@@ -24,14 +26,14 @@ export function ApparenceThemeChange() {
       <CardContent>
         <RadioGroup defaultValue={resolvedTheme} onValueChange={setTheme}>
           <div className="flex flex-col sm:flex-row gap-6">
-            <Label htmlFor="light" className="cursor-pointer text-center">
-              <RadioGroupItem id="light" value="light" className="hidden" />
-              <div
-                className={cn(
-                  'w-full sm:w-48 space-y-2 p-2 bg-[#ecedef] rounded-md ring-2 ring-offset-4 ring-secondary ring-offset-background',
-                  resolvedTheme === 'light' && 'ring-primary',
-                )}
-              >
+            <Label
+              htmlFor="light"
+              className="cursor-pointer text-center [&:has([data-state=checked])>div]:border-primary"
+            >
+              {isMounted && (
+                <RadioGroupItem id="light" value="light" className="sr-only" />
+              )}
+              <div className="w-full sm:w-48 space-y-2 p-2 bg-[#ecedef] rounded-md border-2">
                 <div className="bg-white space-y-1 p-2 rounded-md">
                   <div className="w-full h-2 bg-[#ecedef] rounded-md" />
                   <div className="w-9/12 h-2 bg-[#ecedef] rounded-md" />
@@ -50,14 +52,14 @@ export function ApparenceThemeChange() {
               </div>
               <span className="block mt-4 font-medium">Claro</span>
             </Label>
-            <Label htmlFor="dark" className="cursor-pointer text-center">
-              <RadioGroupItem id="dark" value="dark" className="hidden" />
-              <div
-                className={cn(
-                  'w-full sm:w-48 space-y-2 p-2 bg-[#020617] rounded-md ring-2 ring-offset-4 ring-secondary ring-offset-background',
-                  resolvedTheme === 'dark' && 'ring-primary',
-                )}
-              >
+            <Label
+              htmlFor="dark"
+              className="cursor-pointer text-center [&:has([data-state=checked])>div]:border-primary"
+            >
+              {isMounted && (
+                <RadioGroupItem id="dark" value="dark" className="sr-only" />
+              )}
+              <div className="w-full sm:w-48 space-y-2 p-2 bg-[#020617] rounded-md border-2">
                 <div className="bg-[#1e293b] space-y-1 p-2 rounded-md">
                   <div className="w-full h-2 bg-[#94a3b8] rounded-md" />
                   <div className="w-9/12 h-2 bg-[#94a3b8] rounded-md" />
